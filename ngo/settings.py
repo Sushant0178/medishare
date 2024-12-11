d8c9bf134bd987797f8f3625f252b09d018a4e8d
sushant kokate <91862793+Sushant0178@users.noreply.github.com>
2024-12-11 17:02:06 +0530

    ok

--- ngo/settings.py
+++ ngo/settings.py
@@ -109,6 +109,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 MEDIA_URLS ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 
 
