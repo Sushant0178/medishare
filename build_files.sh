fbf442b27fa2b4c27b65e22dd3993e0e7c3f1b9c
sushant kokate <91862793+Sushant0178@users.noreply.github.com>
2024-12-11 16:46:37 +0530

    ok

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,10 @@
 echo "BUILD START"
 
 # Install necessary packages from requirements.txt
-python3 -m pip install --upgrade pip
-python3 -m pip install -r requirements.txt
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
 
 # Collect static files for production
-python3 manage.py collectstatic --noinput --clear
+python manage.py collectstatic --noinput --clear
 
 echo "BUILD END"
